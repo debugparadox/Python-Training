c5d36cf1e2768adf87642c68c28571f885ed293b
debugparadox <debugparadox@gmail.com>
2024-11-24 22:27:23 +0530

    Basic datatypes in Python

---
+++ 1. Basic Datatypes/basic_datatype.py
@@ -0,0 +1,23 @@
+print(type(1)) # prints data type as integer
+
+print(type(-123)) # prints the data type as integer
+
+print(type(23456789)) # prints the data type as integer
+
+print(type(0.4345)) # prints the data type as floating
+
+print(type(-0.56785678)) # prints the data type as floating
+
+print(type('c')) # prints the data type as string
+
+print(type('debug paradox'))  # prints the data type as string
+
+print(type("123")) # prints the data type as string
+
+print(type('1.234'))  # prints the data type as string
+
+print(type(False))  # prints the data type as boolean
+
+print(type(True))   # prints the data type as boolean
+
+print(type(None))    # prints the data type as None
