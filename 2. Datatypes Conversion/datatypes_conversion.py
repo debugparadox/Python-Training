5a43710c58186c2f0cb5a3ff842fe32505e038c5
debugparadox <debugparadox@gmail.com>
2024-12-01 21:11:26 +0530

    Datatypes conversion in Python

---
+++ 2. Datatypes Conversion/datatypes_conversion.py
@@ -0,0 +1,96 @@
+# 1. Converting a datatype to an Integer datatype
+
+print("Converting Float to Integer")
+print(int(14.56))
+print(int(-95.5643))
+print(int(44.01))
+print(int(0.0))
+print("----------------------------")
+
+print("Converting String to Int")
+print(int("123456"))
+#print(int("debug paradox"))
+#print(int("debugparadox12345"))
+print(int("1"))
+#print(int("12.96"))
+print("----------------------------")
+
+print("Converting Boolean to Int")
+print(int(True))
+print(int(False))
+print("----------------------------")
+print("----------------------------")
+
+
+
+# 2. Converting a datatype to an Float datatype
+
+print("Converting Int to Float")
+print(float(25))
+print(float(-26))
+print(float(0))
+print("----------------------------")
+
+print("Converting String to Float")
+print(float("25.55"))
+print(float("-26.789"))
+print(float("25"))
+# print(float("debug paradox"))
+#print(float("debug paradox123"))
+print("----------------------------")
+
+print("Converting Boolean to Float")
+print(float(True))
+print(float(False))
+print("----------------------------")
+print("----------------------------")
+
+
+# 3. Converting a datatype to an String datatype
+
+print("Converting Int to String")
+print(str(23))
+print(str(0))
+print(str(-2345))
+print(str(-45678))
+print("----------------------------")
+
+print("Converting Float to String")
+print(str(0.0))
+print(str(4.7892))
+print(str(-55.687))
+print(str(111.989))
+print("----------------------------")
+
+print("Converting Boolean to String")
+print(str(True))
+print(str(False))
+
+
+print("----------------------------")
+print("----------------------------")
+
+
+# 4. Converting a datatype to an Boolean datatype
+
+print("Converting Int to Boolean")
+print(bool(123))
+print(bool(-123))
+print(bool(0))
+print("----------------------------")
+
+print("Converting Float to Boolean")
+print(bool(12.33))
+print(bool(-333.444))
+print(bool(0.0))
+print("----------------------------")
+
+print("Converting String to Boolean")
+print(bool("debug paradox"))
+print(bool("True"))
+print(bool("False"))
+print(bool(""))
+
+
+print("----------------------------")
+print("----------------------------")
