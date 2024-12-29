130ac5890dfabfdb216d1f8311a21cc5486bdb15
debugparadox <debugparadox@gmail.com>
2024-12-29 22:19:10 +0530

    Variables in Python

---
+++ 3. Variables/variables.py
@@ -0,0 +1,46 @@
+# variable_name = value
+# int a = 10
+#In python we use Snake Casing
+#Snake Casing (Divide the words based on convinience or pronounication seperated by underscores Eg : person_name)
+a = 10
+print(a)
+print(type(a))
+print("-----------------------")
+
+b = -12.22
+print(b)
+print(type(b))
+print("-----------------------")
+
+c = "Debug Paradox"
+print(c)
+print(type(c))
+print("-----------------------")
+
+d = True
+print(d)
+print(type(d))
+print("-----------------------")
+
+a = "Python Classes"
+print(a)
+print(type(a))
+print("-----------------------")
+
+print("#######################")
+person_name = "Bob"
+person_age = 23
+person_gender = "Male"
+person_is_sportsman = False
+
+print(person_name)
+print(person_age)
+print(person_gender)
+print(person_is_sportsman)
+
+print("-----------------------")
+
+print(id(person_name))
+print(id(person_age))
+print(id(person_gender))
+print(id(person_is_sportsman))
